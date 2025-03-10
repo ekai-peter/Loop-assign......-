8bf6e61f65cdfb0ffbb19fe4ba8a3adcb3a5b8a7
ekai-peter <ekaipeter58@gmail.com>
2025-03-10 07:50:18 -0600

    Add files via upload

---
+++ do while (2).c
@@ -0,0 +1,18 @@
+/*
+do while
+author: ekai peter
+reg no:D33-2442-2023
+*/
+#include<stdio.h>
+
+int main()
+{
+int i=1;// start
+do {
+
+    printf("the number is %d and the cube of the %d is %d \n",i,i,i*i*i);
+    i++;// step
+    }
+    while (i<=100); // step
+    return 0;
+}
