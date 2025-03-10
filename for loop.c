a825780d88d462cde75a08c5966232f78274c6c2
ekai-peter <ekaipeter58@gmail.com>
2025-03-10 07:52:05 -0600

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,16 @@
+/*
+for loop
+author: ekai peter
+reg no. D33-2442-2023
+*/
+#include<stdio.h>
+
+int main()
+{
+int i;
+for (i=1;i<=100;i++){
+
+    printf("the number is %d and cube of %d is %d \n", i,i,i*i*i);
+    }
+    return 0; // successful execution 
+}
---
+++ while loop (1).c
@@ -0,0 +1,17 @@
+/*
+while loop
+author: ekai peter
+reg no:D33-2442-2023
+*/
+#include<stdio.h>
+
+int main()
+{
+int i=1;
+while (i<=100){
+
+    printf("the number is %d and the cube of %d is %d \n", i,i,i*i*i);
+    i++;
+    }
+    return 0;
+}
